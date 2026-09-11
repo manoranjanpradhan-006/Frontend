446e1446030641db8d7f81338e7e4c386888660b
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-09-11 22:27:04 +0530

    .

---
+++ Basic-Frontend/Frontend-1/script.js
@@ -0,0 +1,7 @@
+document.getElementById("notification").addEventListener("click", () => {
+  console.log("hii");
+});
+
+document.getElementById("mic").addEventListener("click", () => {
+  console.log("You clicked ion the mic..");
+});
